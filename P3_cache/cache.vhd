14b7843f37a8684118e1d255e8757e70c6ba53a4
andibakti <andi-camille.bakti@mail.mcgill.ca>
2018-02-28 16:42:16 -0500

    1st thing works

--- P3_cache/cache.vhd
+++ P3_cache/cache.vhd
@@ -32,14 +32,14 @@ end cache;
 architecture arch of cache is
 
 -- declare signals here
-	type mem_array is array(32-1 downto 0) of std_logic_vector(137 downto 0);
-	signal ram: mem_array;
-	signal rowFound : std_logic_vector(137 downto 0);
+	type mem_array is array(32-1 downto 0) of std_logic_vector(135 downto 0);
+	signal ram: mem_array:= (others=> std_logic_vector(to_unsigned(0,136)));
+	signal rowFound : std_logic_vector(135 downto 0);
 	type state_type is (init, default, write_valid, write_invalid, read_valid, read_invalid, read_invalid_dirty);
 	signal next_state, current_state: state_type := init;
 
 
-	signal row : std_logic_vector(137 downto 0);
+	signal row : std_logic_vector(135 downto 0);
 	signal r_counter,  w_counter : integer := 0;
 
 	type read_state_type is (default, reading, done_read);
@@ -49,7 +49,7 @@ architecture arch of cache is
 	signal next_state_write, write_state: write_state_type := default;
 	--bit table
 	----------------------------------------------------------------------
-	--|137 Valid | 136 Dirty | 135   Tag   128 | 127     Block Data     0 |
+	--|135 Valid | 134 Dirty | 133   Tag   128 | 127     Block Data     0 |
 	----------------------------------------------------------------------
 
 
@@ -76,7 +76,7 @@ begin
 
 	--
 	--
-	state_logic: process(current_state, s_addr, s_read, s_write)
+	state_logic: process(current_state, s_addr, s_read, s_write, read_state, write_state, ram)
 	begin
 		case current_state is
 
@@ -84,22 +84,22 @@ begin
 			-- 	Depending on this input and if all the conditions match
 			--	then we determine the correct next state.
 			when init =>
-				for i in 0 to 31 loop
-					ram(i)(137) <= '0';
-				end loop;
+				--for i in 0 to 31 loop
+				----ram(i) <= std_logic_vector(to_unsigned(others=>0));
+				--end loop;
 				next_state <= default;
 
 			when default =>	--do all checks
 
 						s_waitrequest <= '1'; 	--waiting for requests
 
-							rowFound <= ram(to_integer(unsigned(s_addr(8 downto 4))));	-- 5 bits needed to find the correct index in a 4096 bit (word aligned)
+						rowFound <= ram(to_integer(unsigned(s_addr(8 downto 4))));	-- 5 bits needed to find the correct index in a 4096 bit (word aligned)
 
 							if(s_write = '1') then
 
 								--go to write state
-								if(rowFound(135 downto 128) = s_addr(14 downto 9)) then	--check if the tag is the same
-									if(rowFound(137) = '1') then --ie is the bit valid? (1 = a hit)
+								if(rowFound(133 downto 128) = s_addr(14 downto 9)) then	--check if the tag is the same
+									if(rowFound(135) = '1') then --ie is the bit valid? (1 = a hit)
 										next_state <= write_valid;
 									else
 										next_state <= write_invalid;
@@ -109,11 +109,11 @@ begin
 								end if;
 
 							elsif(s_read = '1') then
-								if(rowFound(137) = '1') then --ie is the bit valid? (1 = a hit)
-									if(rowFound(135 downto 128) = s_addr(14 downto 9)) then	--check if the tag is the same
+								if(rowFound(135) = '1') then --ie is the bit valid? (1 = a hit)
+									if(rowFound(133 downto 128) = s_addr(14 downto 9)) then	--check if the tag is the same
 										next_state <= read_valid;
 									else
-										if(rowFound(135) = '1') then --ie is the bit dirty
+										if(rowFound(134) = '1') then --ie is the bit dirty
 											next_state <= read_invalid_dirty;
 										else
 											next_state <= read_invalid;
@@ -129,10 +129,13 @@ begin
 			--	set the wait request to be '0'.
 			--
 			when write_valid =>
-						case (s_addr(3 downto 0)) is
-							when "0000"   => rowFound(31 downto 0)    <= s_writedata;
-							when "0100"   => rowFound(63 downto 32)   <= s_writedata;
-							when "1000"   => rowFound(95 downto 64)   <= s_writedata;
+						
+
+						rowFound <= ram(to_integer(unsigned(s_addr(8 downto 4))));	-- 5 bits needed to find the correct index in a 4096 bit (word aligned)
+						case (s_addr(3 downto 2)) is
+							when "00"   => rowFound(31 downto 0)    <= s_writedata;
+							when "01"   => rowFound(63 downto 32)   <= s_writedata;
+							when "10"   => rowFound(95 downto 64)   <= s_writedata;
 							when others => rowFound(127 downto 96)  <= s_writedata;
 						end case;
 						s_waitrequest <= '0'; 	--data is on the bus
@@ -149,10 +152,10 @@ begin
 
 			when read_valid =>
 						-- select the word to be read
-						case (s_addr(3 downto 0)) is
-							when "0000"   =>  s_readdata <= rowFound(31 downto 0);
-							when "0100"   =>  s_readdata <= rowFound(63 downto 32);
-							when "1000"   =>  s_readdata <= rowFound(95 downto 64);
+						case (s_addr(3 downto 2)) is
+							when "00"   =>  s_readdata <= rowFound(31 downto 0);
+							when "01"   =>  s_readdata <= rowFound(63 downto 32);
+							when "10"   =>  s_readdata <= rowFound(95 downto 64);
 							when others =>  s_readdata <= rowFound(127 downto 96);
 						end case;
 
@@ -177,24 +180,24 @@ begin
 
 
 	-- read fsm for the avalon interface
-	read_process: process(s_addr, m_waitrequest, current_state, m_waitrequest)
+	read_process: process(s_addr, m_waitrequest, current_state, m_waitrequest, m_readdata, ram)
 	begin
 		case read_state is
 			when default =>
 				if (current_state = write_invalid OR current_state = read_invalid) then
 					next_state_read <= reading;
+					m_read <= '1';
+					row <= ram(to_integer(unsigned(s_addr(8 downto 4))));
 				end if;
 			 -- Do nothing
 			when reading =>
-				row <= ram(to_integer(unsigned(s_addr(8 downto 4))));
 				if(r_counter = 16) then
-					row(137) <= '1'; -- Valid
-					row(136) <= '0'; -- Not dirty
-					row(135 downto 128) <= s_addr(14 downto 9); -- Tag
-					ram(to_integer(unsigned(s_addr(8 downto 4)))) <= row;
+					row(135) <= '1'; -- Valid
+					row(134) <= '0'; -- Not dirty
+					row(133 downto 128) <= s_addr(14 downto 9); -- Tag
 					next_state_read <= done_read;
 					r_counter <= 0;		 			 --reset r_counter
-				elsif( m_waitrequest = '0') then
+				elsif( m_waitrequest = '1') then
 					next_state_read <= reading;
 					r_counter <= r_counter + 1;
 					row(8*(1 + r_counter)-1 downto 8*(r_counter)) <= m_readdata; -- 10 is the data offset in the block
@@ -206,6 +209,7 @@ begin
 
 			when done_read =>
 				next_state_read <= default;
+				ram(to_integer(unsigned(s_addr(8 downto 4))))<= row;
 			end case;
 	end process; --end of the read_process
 
--- P3_cache/cache_tb.tcl
+++ P3_cache/cache_tb.tcl
@@ -46,4 +46,4 @@ force -deposit clk 0 0 ns, 1 0.5 ns -repeat 1 ns
 AddWaves
 
 ;# Run for 20 ns
-run 20ns
+run 100ns
