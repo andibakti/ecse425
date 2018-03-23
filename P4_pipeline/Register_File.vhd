5785e42ba87c2730b39c820cf6033d730bf1e1b9
ClemF <ClemF@github.com>
2018-03-23 17:34:42 -0400

    ind

--- P4_pipeline/Register_File.vhd
+++ P4_pipeline/Register_File.vhd
@@ -4,7 +4,7 @@ use IEEE.std_logic_1164.all;
 ENTITY registerfile IS
 
 	PORT (
-    clk,rst : IN std_ulogic;
+	clk,rst : IN std_ulogic;
 	write_en : IN std_ulogic;
     
 	writedata : IN std_ulogic_vector(31 DOWNTO 0);
@@ -22,29 +22,28 @@ END registerfile;
 
 ARCHITECTURE arch OF registerfile IS 
 	
-    TYPE registers IS ARRAY (0 to 31) OF STD_LOGIC_VECTOR(31 downto 0);
+	TYPE registers IS ARRAY (0 to 31) OF STD_LOGIC_VECTOR(31 downto 0);
 	SIGNAL register_file: registers := (OTHERS => "00000000000000000000000000000000"); --initialize all entries of the array
 
 BEGIN
-    
+
 	PROCESS (clk)
 	BEGIN
-    
+	
     	IF (rst = '1') THEN
         	
-            register_file := (OTHERS => "00000000000000000000000000000000");
-    
-		ELSIF (rising_edge(clk)) THEN
-        	    
-            ELSIF (write_en = '1') THEN
-				register_file(to_integer(unsigned(addr_write))) <= rd_data;
-			END IF;
-            
-            
-			read_regA <= register_store(to_integer(unsigned(addr_regA)));
-			read_regB <= register_store(to_integer(unsigned(addr_regB)));
-            
+        	register_file := (OTHERS => "00000000000000000000000000000000");
+        	
+        ELSIF (rising_edge(clk)) THEN
+	
+		IF (write_en = '1') THEN
+			register_file(to_integer(unsigned(addr_write))) <= rd_data;
 		END IF;
+		
+		read_regA <= register_store(to_integer(unsigned(addr_regA)));
+		read_regB <= register_store(to_integer(unsigned(addr_regB)));
+		
+	END IF;
 	END PROCESS;
     
 END arch;
