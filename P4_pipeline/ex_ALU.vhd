e94ba691edda73d3b415a9cd4e46ae553c1c1d87
Chloe Grosdidier <jawaialler@gmail.com>
2018-03-28 16:28:43 -0400

    Updated ALU, got a shitload of inputs

--- P4_pipeline/ex_ALU.vhd
+++ P4_pipeline/ex_ALU.vhd
@@ -7,23 +7,33 @@ entity ex_ALU is
     port(
         clock, rst: in std_logic;
         a: in std_logic_vector(31 downto 0);
-	b: in std_logic_vector(31 downto 0);
-	signExtendImmediate: in std_logic_vector(31 downto 0);
-   uSignExtendImmediate: in std_logic_vector(31 downto 0);
-	sel: in std_logic_vector(5 downto 0);
-	funct: in std_logic_vector(5 downto 0);
+		b: in std_logic_vector(31 downto 0);
+		address_in: in std_logic_vector(25 downto 0);
+		offset_in: in std_logic_vector(15 downto 0);
+		shift_in: in std_logic_vector(5 downto 0);
+		signExtendImmediate: in std_logic_vector(31 downto 0);
+    	uSignExtendImmediate: in std_logic_vector(31 downto 0);
+		sel: in std_logic_vector(5 downto 0);
+		funct: in std_logic_vector(5 downto 0);
+		pc_in: in std_logic_vector(32 downto 0);
+		regWrite_in: in std_logic_vector(4 downto 0);
 
-        zero: out std_logic;
-        output: out std_logic_vector(31 downto 0)
-        );
+        jump: out std_logic;
+        mem: out std_logic;
+        load: out std_logic;
+        store: out std_logic;
+        jumpAddress: out std_logic(31 downto 0);
+        memAddress: out std_logic(31 downto 0);
+        regWrite_out: out std_logic_vector(4 downto 0);
+        result: out std_logic_vector(31 downto 0)
+   	);
 end entity;
 
 architecture arch of ex_ALU is
 --declare signals
-signal temp: std_logic_vector(31 downto 0);
+signal temp,jA: std_logic_vector(31 downto 0);
 signal hi, lo: std_logic_vector(31 downto 0);
-signal stall : std_logic;
-
+signal memoryCheck,jumpCheck,storeCheck,loadCheck: std_logic;
 
 begin
     process (clock) begin
@@ -46,11 +56,11 @@ begin
 						when "100011" =>
 							temp <= a xor b;--xor
 						when "000000" =>
-							temp <= std_logic_vector(shift_left(unsigned(a), to_integer(signed(b)))); --shift left logical (unsigned)
+							temp <= std_logic_vector(shift_left(unsigned(a), to_integer(unsigned(shift_in)))); --shift left logical (unsigned)
 						when "000010" =>
-							temp <= std_logic_vector(shift_right(unsigned(a), to_integer(signed(b)))); --shift right logical
+							temp <= std_logic_vector(shift_right(unsigned(a), to_integer(unsigned(shift_in)))); --shift right logical
 						when "000011" =>
-							temp <= std_logic_vector(shift_right(signed(a), to_integer(signed(b)))); --shift right arithmetic (signed)
+							temp <= std_logic_vector(shift_right(signed(a), to_integer(signed(shift_in)))); --shift right arithmetic (signed)
 						when "011010" =>
 							lo <= std_logic_vector(signed(a)/signed(b)); --div
 							hi <= std_logic_vector(signed(a) mod signed(b)); --div
@@ -60,12 +70,21 @@ begin
 							temp <= hi;
 						when "010010" =>
 							temp <= lo;
+						when "001000" => --jump register
+							jumpCheck <= '1'; 
+							jA <= b;
 						when "101010" => --set less than (slt)
 							if(signed(a) < signed(b)) then
 								temp <= X"00000001";
 							else
 								temp <= (others => '0');
 							end if;
+						when others =>
+							temp <= (others => '0');
+							jumpCheck <= '0';
+							memCheck <= '0';
+							loadCheck <= '0';
+							storeCheck <= '0';
 					end case;
 
 				when "001000" =>
@@ -84,30 +103,50 @@ begin
 					temp <= a xor uSignExtendImmediate; --xor immediate
 				when "001111" =>
 					temp <= std_logic_vector(shift_left(signed(a),16)); --load upper immediate
+				when "000100" =>  --branch on equal (beq)
+					if(signed(a) = signed(b)) then
+						jumpCheck <= '1';
+					else
+						jumpCheck <= '0';
+					end if;
+					jA <= std_logic_vector(signed(offset_in) + unsigned(pc_in));
+				when "000101" => --branch on not equal (beq)
+					if(signed(a) = signed(b)) then
+						jumpCheck <= '0';
+					else
+						jumpCheck <= '1';
+					end if;
+					jA <= std_logic_vector(signed(offset_in) + unsigned(pc_in));
+				when "000010" => --jump
+					jumpCheck <= '1'; 
+					jA <= address_in;
+				when "000011" => --jump and link
+					jumpCheck <= '1';
+					jA <= address_in;
+				when "100011" => --load word (lw)
+					memCheck <= '1';
+					loadCheck <= '1';
+					storeCheck <= '0';
+				when "101011" => --store word (sw)
+					memCheck <= '1';
+					storeCheck <= '1';
+					loadCheck <= '0';
 				when others =>
 					temp <= (others => '0');
-					-- stall
-					stall <= '1';
-
-				--when "????" =>  --branch on equal (beq)
-				--		if(signed(a) = signed(b)) then
-				--			temp <= ;
-				--		else
-				--			temp <= ;
-				--		end if;
-				--when "????" => temp <= ???? --branch on not equal (beq)
-				--when "????" => temp <= ???? --jump
-				--when "????" => temp <= ???? --jump register
-				--when "????" => temp <= ???? --jump and link
-
-				--when "????" => temp <= ???? --load word (lw)
-				--when "????" => temp <= ???? --store word (sw)
-
+					jumpCheck <= '0';
+					memCheck <= '0';
+					loadCheck <= '0';
+					storeCheck <= '0';
 			end case;
 
         end if;
     end process;
-output <= temp;
-zero <= stall;
+result <= temp;
+jump <= jumpCheck;
+jumpAddress <= jA;
+mem <= memCheck;
+store <= storeCheck;
+load <= loadCheck;
+regWrite_out <= regWrite_in;
 
 end arch;
