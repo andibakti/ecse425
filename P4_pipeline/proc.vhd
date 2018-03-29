215e8529c6b65e0715a4876636eca27cf7afa668
andibakti <andi-camille.bakti@mail.mcgill.ca>
2018-03-29 18:09:07 -0400

    Create pot maps

--- P4_pipeline/proc.vhd
+++ P4_pipeline/proc.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity proc is
+--Todo
 end proc;
 
 architecture arch of proc is
@@ -44,15 +45,6 @@ port(
 	input: in std_logic_vector(31 downto 0);
 	reset: in std_logic;
 	output: out std_logic_vector(31 downto 0)
-);
-end component;
-
-component mux_2to1 is
-port(
-	SEL : in  STD_LOGIC;
-	A : in  STD_LOGIC_VECTOR (31 downto 0);
-	B : in  STD_LOGIC_VECTOR (31 downto 0);
-	X : out STD_LOGIC_VECTOR (31 downto 0)
 );
 end component;
 
@@ -147,6 +139,8 @@ port(
 );
 end component;
 
+signal clk: std_logic;
+signal rst:std_logic;
 
 -- test signals
 signal data_in_usign_ext : std_logic_vector(15 downto 0);
@@ -155,18 +149,16 @@ signal data_out_usign_ext : std_logic_vector(31 downto 0);
 signal data_in_sign_ext : std_logic_vector(15 downto 0);
 signal data_out_sign_ext : std_logic_vector(31 downto 0);
 
-signal clk_reg_file, rst_reg_file, write_en_reg_file: std_logic;
+signal write_en_reg_file: std_logic;
 signal writedata_reg_file: std_logic_vector(31 downto 0);
 signal addr_write_reg_file, addr_regA_reg_file, addr_regB_reg_file: std_logic_vector(4 downto 0);
 signal read_regA_reg_file, read_regB_reg_file: std_logic_vector(15 downto 0);
 
-signal override_pc_add: std_logic;
-signal pc_pc_add, override_pc_pc_add: std_logic_vector(31 downto 0);
+signal override: std_logic;
+signal pc, override_pc: std_logic_vector(31 downto 0);
 signal output_pc_add: std_logic_vector(31 downto 0);
 
-signal clock_pc: std_logic;
 signal input_pc: std_logic_vector(31 downto 0);
-signal reset_pc: std_logic;
 signal output_pc: std_logic_vector(31 downto 0); 
 
 signal SEL_mux:  STD_LOGIC;
@@ -174,7 +166,6 @@ signal A_mux :  STD_LOGIC_VECTOR (31 downto 0);
 signal B_mux :  STD_LOGIC_VECTOR (31 downto 0);
 signal X_mux :  STD_LOGIC_VECTOR (31 downto 0);
 
-signal clock_instr_mem:  std_logic;
 signal writedata_instr_mem:  std_logic_vector (31 downto 0);
 signal address_instr_mem:  std_logic_vector (31 downto 0);
 signal memwrite_instr_mem:  std_logic;
@@ -182,7 +173,6 @@ signal memread_instr_mem:  std_logic;
 signal readdata_instr_mem:  std_logic_vector (31 downto 0);
 signal waitrequest_instr_mem:  std_logic;
 
-signal clock,rst_id_reg:	 std_logic;
 signal pc_in_id_reg:		 std_logic_vector(31 downto 0);
 signal instruction_in_id_reg:  std_logic_vector(31 downto 0);
 signal reg_write_out_id_reg:  std_logic_vector(4 downto 0);
@@ -202,7 +192,6 @@ signal regB_id_hazard_dect : std_logic_vector (4 downto 0);
 signal regA_id_hazard_dect : std_logic_vector (4 downto 0);
 signal hazOut_hazard_dect : std_logic;
 
-signal clock_ex_alu, rst_ex_alu: std_logic;
 signal a_ex_alu: std_logic_vector(31 downto 0);
 signal b_ex_alu: std_logic_vector(31 downto 0);
 signal address_in_ex_alu: std_logic_vector(25 downto 0);
@@ -224,7 +213,6 @@ signal regWrite_out_ex_alu: std_logic_vector(4 downto 0);
 signal result_ex_alu: std_logic_vector(31 downto 0);
 
 
-signal clock_data_mem: std_logic;
 signal data_in_data_mem: std_logic_vector(31 downto 0):=(others => '0');
 signal do_load_data_mem: std_logic := '0';
 signal do_write_data_mem: std_logic := '0';
@@ -236,17 +224,136 @@ signal reg_id_out_data_mem:  std_logic_vector(4 downto 0);
 
 
 
-
 --signal s_addr : std_logic_vector (31 downto 0);
 
 begin
 usign_ext_instance: usign_Ext
 port map(
+	data_in => data_in_usign_ext,
+	data_out => data_out_usign_ext
 	);
 
 sign_ext_instance: sign_Ext
 port map(
+	data_in => data_in_sign_ext,
+	data_out => data_out_sign_ext
 	);
+
+register_file_instance: register_file
+port map(
+	clk => clk,
+	rst => rst,
+	write_en => write_en_reg_file,
+	writedata => writedata_reg_file,
+	addr_write => addr_write_reg_file,
+	addr_regA => addr_regA_reg_file,
+	addr_regB => addr_regB_reg_file,
+	read_regA => read_regA_reg_file,
+	read_regB => read_regB_reg_file
+	);
+
+pc_adder_instance: pc_adder
+port map(
+	override => override,
+	pc => pc,
+	override_pc => override_pc,
+	output => output_pc_add
+	);
+
+pc_instance: pc
+port map(
+	clock => clk,
+	input => input_pc,
+	reset => rst,
+	output => output_pc
+	);
+
+mux_2to1_instance: mux_2to1
+port map(
+	sel => SEL_mux,
+	a => A_mux,
+	b => B_mux,
+	x => X_mux
+	);
+
+instr_memory_instance: instr_memory
+port map(
+	clock => clk,
+	writedata => writedata_instr_mem,
+	address => address_instr_mem,
+	memwrite => memwrite_instr_mem,
+	memread => memread_instr_mem,
+	readdata => readdata_instr_mem,
+	waitrequest => waitrequest_instr_mem
+	);
+
+id_reg_instance: id_reg
+port map(
+	clock => clk,
+	rst => rst,
+	pc_in => pc_in_id_reg,
+	instruction_in => instruction_in_id_reg,
+	opCode_out => opCode_out_id_reg,
+	reg1_out => reg1_out_id_reg,
+	reg2_out => reg2_out_id_reg,
+	reg_write_in => reg_write_in_id_reg,
+	address_out =>address_out_id_reg,
+	immediateValue_out => immediateValue_out_id_reg,
+	shamt_out => shamt_out_id_reg,
+	funct_out => funct_out_id_reg,
+	reg_write_out => reg_write_out_id_reg,
+	pc_out => pc_out_id_reg
+	);
+
+hazard_detection_instance: hazard_detection
+port map(
+	en => EN_hazard_dect,
+	regA_ex => regA_ex_hazard_dect,
+	regB_id => regB_id_hazard_dect,
+	hazOut => hazOut_hazard_dect
+	);
+
+ex_alu_instance: ex_ALUx
+port map(
+	clock => clk,
+	rst => rst,
+	a => a_ex_alu,
+	b => b_ex_alu,
+	address_in => address_in_ex_alu,
+	offset_in => offset_in_ex_alu,
+	shift_in  => shift_in_ex_alu,
+	signExtendImmediate => signExtendImmediate_ex_alu,
+	uSignExtendImmediate => uSignExtendImmediate_ex_alu,
+	sel => sel_ex_alu,
+	funct => funct_ex_alu,
+	pc_in => pc_in_ex_alu,
+	regWrite_in => regWrite_in_ex_alu,
+	jump => jump_ex_alu,
+	mem => mem_ex_alu,
+	load => load_ex_alu,
+	store => store_ex_alu,
+	jumpAddress => jumpAddress_ex_alu,
+	memAddress => memAddress_ex_alu,
+	regWrite_out => regWrite_out_ex_alu,
+	result => result_ex_alu
+	);
+
+data_memory_instance: data_memory
+port map(
+	clock => clk,
+	data_in => data_in_data_mem,
+	do_load => do_load_data_mem,
+	do_write => do_write_data_mem,
+	writeMem => writeMem_data_mem,
+	addr => addr_data_mem,
+	reg_id_in => reg_id_in_data_mem,
+	data_out => data_out_data_mem,
+	reg_id_out => reg_id_out_data_mem
+	);
+
+test_process: process
+begin
+end process;
 
 
 end;
