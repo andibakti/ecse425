7408294c357a8250c227dbe03f88948a762907a8
ClemF <ClemF@github.com>
2018-04-16 18:44:22 -0400

    Fibonacci test asm

--- P5_optimized/P4_assembler/assembler/Testbench/Test1NoBranch.asm
+++ P5_optimized/P4_assembler/assembler/Testbench/Test1NoBranch.asm
@@ -1,17 +1,16 @@
 
-#this program will test basic register instructions : add, sub, and, nor, or, xor, shiftleft, shiftright, div, mul
+#this program will test basic register instructions : add, sub, and, or, shiftleft, shiftright, div, mult
 
-addi $1, $0, 10  #reg 1 = 10
-addi $2, $0, 2   #reg2 = 2
-add $3, $1, $2   #reg3 = 12
-sub $4, $3, $2   #reg4 = 10
-mult $1, $2      #lo = 20
-mflo $5          #reg5 = 20
-div $3, $2   #lo = 6
-mflo $6          #reg6 = 6
-sll $7, $2, 4    #reg7 = 32
-srl $8, $2, 1    #reg8 = 1
-or $1, $1, $5        #reg1 = 30
-addi $9, $0, 31  #reg9 = 31
-and $9, $9, $2       #reg9 = 2
-
+addi $1, $0, 10     #reg 1 = 10
+addi $2, $0, 2      #reg2 = 2
+add $3, $1, $2      #reg3 = 12
+sub $4, $3, $2      #reg4 = 10
+mult $1, $2         #lo = 20
+mflo $5             #reg5 = 20
+div $3, $2          #lo = 6
+mflo $6             #reg6 = 6
+sll $7, $2, 4       #reg7 = 32
+srl $8, $2, 1       #reg8 = 1
+or $1, $1, $5       #reg1 = 30
+addi $9, $0, 31     #reg9 = 31
+and $9, $9, $2      #reg9 = 2
---
+++ P5_optimized/P4_assembler/assembler/Testbench/Test2Fibonacci.asm
@@ -0,0 +1,20 @@
+
+
+addi $10, $0, 8           # Stores the number of generated number desired
+addi $1, $0, 1            # F(0) = 1, reg 1 always store F(n-1)
+addi $2, $0, 1            # F(1) = 1, reg 2 always stores F(n)
+addi $11, $0, 3000        # store address in mem
+addi $15, $0, 4           # word size
+
+loop : addi $3, $2, 0     # temp = old F(n)
+	add $2, $2, $1        # update F(n)
+	addi $1, $3, 0        # update F(n-1)
+	mult $10, $15         # computes by how much data pointer is shifted
+	mflo $12
+	add $13, $11, $12     # update data pointer
+	sw $2, 0($13)         # stores F(n) in mem
+	addi $10, $10, -1     # decrease counter
+	bne $10, $0, loop     # loop condition
+	
+
+	
